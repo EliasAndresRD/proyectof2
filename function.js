fe565c7d558e9eed081204f0714f2e4ae49d3763
EliasAndresRD <126621822+EliasAndresRD@users.noreply.github.com>
2023-03-17 17:33:17 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+function boton() {
+    var x = document.getElementById("productos_vendidos");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
